cef6484c433606cf8d91e4f0de122ac1eb70af05
Yurii Rybachok <drummer2009@ukr.net>
2023-06-15 18:08:49 +0300

    show figure when add wrong letter

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ window.addEventListener('keydown', ({key}) => {
   {{ wrongLetters }}
   <GameHeader />
   <div class="game-container">
-    <GameFigure />
+    <GameFigure :wrong-letters-count="wrongLetters.length" />
     <GameWrongLetters :wrong-letters="wrongLetters" />
     <GameWord :word="word" :correct-letters="correctLetters" />
   </div>
