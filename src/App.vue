6a88c4998618036608e270a2522186a061b06944
Yurii Rybachok <drummer2009@ukr.net>
2023-06-15 08:37:58 +0300

    popup component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import GameHeader from './components/GameHeader.vue';
 import GameFigure from './components/GameFigure.vue';
 import GameWrongLetters from './components/GameWrongLetters.vue';
 import GameWord from './components/GameWord.vue';
+import GamePopup from './components/GamePopup.vue';
 </script>
 
 <template>
@@ -13,15 +14,7 @@ import GameWord from './components/GameWord.vue';
     <GameWord />
   </div>
 
-  <!-- Container for final message -->
-  <div v-if="false" class="popup-container">
-    <div class="popup">
-        <h2>Поздравляю, вы победили! 😃</h2>
-        <!-- <h2>Вы проиграли. 😕</h2> -->
-      <!-- <h3>...имя: Лидия</h3> -->
-      <button>Сыграть еще раз</button>
-    </div>
-  </div>
+  <GamePopup />
 
   <!-- Notification -->
   <div class="notification-container show">
